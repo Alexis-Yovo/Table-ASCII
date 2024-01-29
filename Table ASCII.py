f05750da4f60312f6365793c517356561c502146
Alexis-Yovo <74179398+Alexis-Yovo@users.noreply.github.com>
2024-01-29 13:48:40 +0100

    Add files via upload

---
+++ Table ASCII.py
@@ -0,0 +1,7 @@
+import random
+random.randint(10,255)
+for myChar in range(10):
+myChar = input(randomNum)
+print ("Voici les caractères du code ASCII:")
+print('Le code'+ str(randomNum) 'correspond au' + myChar + '\n' ) 
+
